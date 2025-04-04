edb9d3ba15ace114bede749cbd40f11d699a99f9
phsemple <patrick.harley608@gmail.com>
2025-04-03 19:13:10 -0500

    Correct root password

--- src/model/connectDB.js
+++ src/model/connectDB.js
@@ -15,7 +15,7 @@ async function connectDB() {
         console.log(`Connecting to MySQL database...    S
                    host: ${process.env.MYSQLHOST} 
                    user: ${process.env.MYSQLUSER}
-                    password: ${process.env.MYSQL_PWD}   
+                    password: ${process.env.MYSQL_ROOT_PASSWORD}   
                    database: ${process.env.MYSQLDATABASE}`);
         
         // Create Connection Object
